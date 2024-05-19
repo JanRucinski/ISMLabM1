874b3dfdc882d67659f30f27461be2086fef22e8
JanRucinski <jasislav@gmail.com>
2024-05-19 23:24:13 +0200

    lab6 + lab4fix

--- com.rucinski/src/main/java/com/example/com_rucinski/aspects/StatisticsAspect.java
+++ com.rucinski/src/main/java/com/example/com_rucinski/aspects/StatisticsAspect.java
@@ -1,2 +1,31 @@
-package com.example.com_rucinski.aspects;public class StatisticsAspect {
+package com.example.com_rucinski.aspects;
+
+import com.example.model.EventDTO;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Aspect
+@Component
+public class StatisticsAspect {
+
+    private AtomicInteger getAllEventsCallCount = new AtomicInteger(0);
+
+    @Pointcut("execution(* com.example.com_rucinski.service.EventService.getAllEvents(..))")
+    public void getAllEventsPointcut() {}
+
+    @AfterReturning(pointcut = "getAllEventsPointcut()", returning = "result")
+    public void afterReturningGetAllEvents(List<EventDTO> result) {
+        getAllEventsCallCount.incrementAndGet();
+    }
+
+
+    public int getGetAllEventsCallCount() {
+        return getAllEventsCallCount.get();
+    }
 }
+
--- com.rucinski/src/main/java/com/example/com_rucinski/aspects/StatisticsController.java
+++ com.rucinski/src/main/java/com/example/com_rucinski/aspects/StatisticsController.java
@@ -1,2 +1,26 @@
-package com.example.com_rucinski.aspects;public class StatisticsController {
+package com.example.com_rucinski.aspects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatisticsController {
+
+    @Autowired
+    private StatisticsAspect statisticsAspect;
+
+    @GetMapping("/api/statistics")
+    public ResponseEntity getStatistics() {
+        return ResponseEntity.ok(statisticsAspect.getGetAllEventsCallCount());
+    }
+}
+
+class StatisticsDTO {
+    private int getAllEventsCallCount;
+
+    public StatisticsDTO(int getAllEventsCallCount) {
+        this.getAllEventsCallCount = getAllEventsCallCount;
+    }
 }
